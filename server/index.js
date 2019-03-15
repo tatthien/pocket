32e83a41052bde32ba20d5754bed340827ebcf97
Thien Nguyen <tatthien.contact@gmail.com>
2019-03-15 16:05:24 +0700

    add config file for server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const mysql = require('mysql')
 const bodyParser = require('body-parser')
+const yaml = require('js-yaml')
+const fs = require('fs')
+const path = require('path')
 const transactions = require('./routes/transactions')
 const reports = require('./routes/reports')
 const auth = require('./routes/auth')
@@ -8,16 +11,19 @@ const auth = require('./routes/auth')
 const app = express()
 app.use(bodyParser.json())
 
+// Read config
+let config = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, 'config.yml')), 'utf8')
+
 const db = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: '',
-  database: 'finance'
+  host: config.database.host,
+  user: config.database.username,
+  password: config.database.password,
+  database: config.database.name
 })
 
 db.connect((err) => {
   if (err) {
-    throw err
+    console.log('Cannot connect to database')
   }
 
   console.log('Connected to database')
@@ -28,4 +34,4 @@ app.use('/api/transactions', transactions)
 app.use('/api/reports', reports)
 app.use('/api/auth', auth)
 
-app.listen(3000, () => console.log('Server is running...'))
+app.listen(config.port || 3000, () => console.log(`Server is running on port ${config.port}`))
